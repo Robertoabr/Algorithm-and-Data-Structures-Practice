aadacabca3b508cfd02445095b44afe63a171428
Robertoabr <roberto.brillembourg@gmail.com>
2019-07-30 17:40:49 -0400

    first setup commit

---
+++ highestProductOfThree.js
@@ -0,0 +1,11 @@
+// Given an array of integers, find the highest product you can get from three of the integers.
+//The input arrayOfInts will always have at least three integers.
+
+const arrOfInts = [3, 2, 7];
+
+function highestProductOfThreeInts(array) {
+  let highestProduct = 99;
+  return highestProduct;
+}
+
+console.log(highestProductOfThreeInts(arrOfInts));
