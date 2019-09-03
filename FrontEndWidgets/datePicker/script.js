9164c7e697aa59617f0f1f6d5791a222bd0c5f7c
Robertoabr <roberto.brillembourg@gmail.com>
2019-09-03 11:57:12 -0400

    half way though date-picker mini app

---
+++ FrontEndWidgets/datePicker/script.js
@@ -0,0 +1,95 @@
+let months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
+];
+
+let curMonth = new Date().getMonth();
+let curYear = new Date().getFullYear();
+populateMonthAndYear(curMonth, curYear);
+
+let input = document.querySelector('input');
+
+input.addEventListener('beforeinput', showDatePicker);
+
+function showDatePicker() {}
+
+function changeDate(monthChange, yearChange) {
+  let dispMonth = document.querySelector('.month-section p').innerHTML;
+  let monthIndex = months.indexOf(dispMonth);
+  let dispYear = document.querySelector('.year-section p').innerHTML;
+
+  populateMonthAndYear(
+    (120 + monthIndex + monthChange) % 12,
+    dispYear + yearChange
+  );
+}
+
+function populateMonthAndYear(month, year) {
+  //update Month and Year selectors
+  let yearEl = document.querySelector('.year-section p');
+  let monthEl = document.querySelector('.month-section p');
+  yearEl.innerHTML = year;
+  monthEl.innerHTML = months[month];
+
+  //update table section
+  let weekdayThatMonthBegins = new Date(year, month).getDay();
+  let daysInMonth = 32 - new Date(year, month, 32).getDate();
+  let currentDay = 1;
+
+  //reset table
+  let table = document.querySelector('.day-table');
+  resetTable();
+
+  //update rows
+  for (let rows = 0; rows < 6; rows++) {
+    let row = document.createElement('tr');
+    for (let day = 0; day < 7; day++) {
+      if (rows === 0 && day < weekdayThatMonthBegins) {
+        let cell = document.createElement('td');
+        row.appendChild(cell);
+      } else if (currentDay <= daysInMonth) {
+        let cell = document.createElement('td');
+        let button = document.createElement('button');
+        button.innerHTML = currentDay;
+        cell.appendChild(button);
+        row.appendChild(cell);
+        currentDay++;
+      } else {
+        break;
+      }
+    }
+    table.appendChild(row);
+  }
+}
+
+function resetTable() {
+  let table = document.querySelector('.day-table');
+  table.innerHTML = '';
+  let firstRow = document.createElement('tr');
+  let sun = document.createElement('th');
+  sun.innerHTML = 'Sun';
+  let mon = document.createElement('th');
+  mon.innerHTML = 'Mon';
+  let tue = document.createElement('th');
+  tue.innerHTML = 'Tue';
+  let wed = document.createElement('th');
+  wed.innerHTML = 'Wed';
+  let thu = document.createElement('th');
+  thu.innerHTML = 'Thu';
+  let fri = document.createElement('th');
+  fri.innerHTML = 'Fri';
+  let sat = document.createElement('th');
+  sat.innerHTML = 'Sat';
+  firstRow.append(sun, mon, tue, wed, thu, fri, sat);
+  table.appendChild(firstRow);
+}
